25607829b0f4623a14b07d1131a0a6e3e98a9ecf
Mohamed Hamed <mo7amed.7amedenho@gmail.com>
2025-07-28 18:35:27 +0300

    إصلاح Admin Create User

--- src/app/dashboard/admin/create-user/page.tsx
+++ src/app/dashboard/admin/create-user/page.tsx
@@ -36,77 +36,94 @@ export default function CreateUserPage() {
   };
 
   return (
-    <div className="max-w-xl mx-auto px-4 py-6">
-      <Title level={3} className="text-center">
-        إنشاء مستخدم جديد
-      </Title>
-
+    <div className="mx-auto px-6 py-6">
       <Form
         layout="vertical"
         onFinish={onFinish}
         initialValues={{ permissions: [], warehouses: [] }}
+        className="border rounded-lg shadow-md p-6"
+        style={{ margin: "0 auto" }}
+        autoComplete="off"
       >
-        <Form.Item
-          name="firstName"
-          label="الاسم الأول"
-          rules={[{ required: true, message: "يرجى إدخال الاسم الأول" }]}
-        >
-          <Input />
-        </Form.Item>
+        <Title level={3} className="text-center mt-4">
+          إنشاء مستخدم جديد
+        </Title>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 !pt-10 ">
+          <div className="col-span-1 p-2">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+              <Form.Item
+                name="firstName"
+                label="الاسم الأول"
+                rules={[{ required: true, message: "يرجى إدخال الاسم الأول" }]}
+              >
+                <Input />
+              </Form.Item>
 
-        <Form.Item
-          name="lastName"
-          label="الاسم الأخير"
-          rules={[{ required: true, message: "يرجى إدخال الاسم الأخير" }]}
-        >
-          <Input />
-        </Form.Item>
+              <Form.Item
+                name="lastName"
+                label="الاسم الأخير"
+                rules={[{ required: true, message: "يرجى إدخال الاسم الأخير" }]}
+              >
+                <Input />
+              </Form.Item>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+              <Form.Item
+                name="email"
+                label="البريد الإلكتروني"
+                rules={[{ required: true, message: "يرجى إدخال الإيميل" }]}
+              >
+                <Input type="email" />
+              </Form.Item>
 
-        <Form.Item
-          name="email"
-          label="البريد الإلكتروني"
-          rules={[{ required: true, message: "يرجى إدخال الإيميل" }]}
-        >
-          <Input type="email" />
-        </Form.Item>
+              <Form.Item
+                name="password"
+                label="كلمة المرور"
+                rules={[{ required: true, message: "يرجى إدخال كلمة المرور" }]}
+              >
+                <Input.Password />
+              </Form.Item>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+              <Form.Item
+                name="role"
+                label="الدور الوظيفي"
+                rules={[
+                  { required: true, message: "يرجى إدخال الدور الوظيفي" },
+                ]}
+              >
+                <Input placeholder="مثلاً: مدير حسابات" />
+              </Form.Item>
+              <Form.Item
+                name="department"
+                label="القسم"
+                rules={[{ required: true, message: "يرجى إدخال القسم" }]}
+              >
+                <Input placeholder="مثلاً: الحسابات، المبيدات، المشتل..." />
+              </Form.Item>
+            </div>
+          </div>
 
-        <Form.Item
-          name="password"
-          label="كلمة المرور"
-          rules={[{ required: true, message: "يرجى إدخال كلمة المرور" }]}
-        >
-          <Input.Password />
-        </Form.Item>
-
-        <Form.Item
-          name="role"
-          label="الدور الوظيفي"
-          rules={[{ required: true, message: "يرجى إدخال الدور الوظيفي" }]}
-        >
-          <Input placeholder="مثلاً: مدير حسابات" />
-        </Form.Item>
-
-        <Form.Item
-          name="department"
-          label="القسم"
-          rules={[{ required: true, message: "يرجى إدخال القسم" }]}
-        >
-          <Input placeholder="مثلاً: الحسابات، المبيدات، المشتل..." />
-        </Form.Item>
-
-        <Form.Item name="permissions" label="الصلاحيات">
-          <Checkbox.Group options={permissionsList} />
-        </Form.Item>
-
-        <Form.Item name="warehouses" label="المخازن المسموح بها">
-          <Checkbox.Group options={warehouseOptions} />
-        </Form.Item>
-
-        <Form.Item>
-          <Button type="primary" htmlType="submit" loading={loading} block>
-            إنشاء المستخدم
-          </Button>
-        </Form.Item>
+          <div>
+            <Form.Item name="permissions" label="الصلاحيات">
+              <Checkbox.Group options={permissionsList} />
+            </Form.Item>
+            <Form.Item name="warehouses" label="المخازن المسموح بها">
+              <Checkbox.Group options={warehouseOptions} />
+            </Form.Item>
+            <Form.Item>
+              <Button
+                type="primary"
+                className="w-md max-w-md"
+                htmlType="submit"
+                loading={loading}
+                block
+              >
+                إنشاء المستخدم
+              </Button>
+            </Form.Item>
+          </div>
+        </div>
       </Form>
     </div>
   );
