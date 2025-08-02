1584098047ad08ae3ebde8e0099f63796bb18f22
Mohamed Hamed <mo7amed.7amedenho@gmail.com>
2025-08-02 19:19:53 +0300

    تعديل prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,3 +1,14 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
+// src/lib/prisma.ts
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+const prisma = global.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  global.prisma = prisma;
+}
+
 export default prisma;
